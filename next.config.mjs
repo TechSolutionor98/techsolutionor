709e267d240a55392a2e85c2c0d16f3b90330901
saleemmughal17 <saleem177mughal@gmai.com>
2026-08-28 18:57:21 +0500

    fixed this again run & run project

--- next.config.mjs
+++ next.config.mjs
@@ -24,6 +24,15 @@ const nextConfig = {
       },
     ],
   },
+  webpack: (config, { dev }) => {
+    if (dev) {
+      config.watchOptions = {
+        poll: 800,
+        aggregateTimeout: 300,
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
